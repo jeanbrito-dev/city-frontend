817d913282c8a6501f11bf1ed47a4b749409dc9b
Brito <jean.gustavo078@gmail.com>
2026-04-18 15:37:56 -0300

    mapa conectado a api

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
@@ -5,27 +6,21 @@ import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Dados mock
-const occurrences = [
-  {
-    id: 1,
-    titulo: "Buraco na rua",
-    descricao: "Grande buraco na avenida",
-    status: "urgente",
-    position: [-23.6205, -45.4132],
-  },
-  {
-    id: 2,
-    titulo: "Lixo acumulado",
-    descricao: "Muito lixo na calçada",
-    status: "em análise",
-    position: [-23.622, -45.41],
-  },
-];
+import { getOccurrences } from "../services/api";
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+});
 
 export default function Map() {
+  const [occurrences, setOccurrences] = useState([]);
+
+  useEffect(() => {
+    getOccurrences().then(setOccurrences);
+  }, []);
+
   return (
-    <div className="h-[500px] w-full rounded-xl overflow-hidden">
+    <div className="h-125 w-full rounded-xl overflow-hidden">
       <MapContainer
         center={[-23.6205, -45.4132]}
         zoom={13}
@@ -37,7 +32,10 @@ export default function Map() {
         />
 
         {occurrences.map((occ) => (
-          <Marker key={occ.id} position={occ.position}>
+          <Marker
+            key={occ.id}
+            position={[Number(occ.latitude), Number(occ.longitude)]} // garante número
+          >
             <Popup>
               <strong>{occ.titulo}</strong>
               <p>{occ.descricao}</p>
