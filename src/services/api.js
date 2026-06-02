48cfa212116980e5fca34eac3dbe4139ca2ebfc7
andradejvc <joaovictorcamargoandrade@gmail.com>
2026-06-02 15:17:36 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://city-backend-production.up.railway.app"
 
 const apiFetch = async (url, options = {}) => {
   const token = localStorage.getItem("token");
